e7bea89d80984f660ebdf472fa596b5e8267641b
aleaguiard <aleaguiard>
2024-01-20 18:30:06 +0000

    Cambio nombre paquetes

--- app/src/androidTest/java/com/aad/mytodolist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aad/mytodolist/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.mytodolist;
+package com.aad.mytodolist;
 
 import android.content.Context;
 
@@ -16,11 +16,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class aadInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.mytodolist", appContext.getPackageName());
+        assertEquals("com.aad.mytodolist", appContext.getPackageName());
     }
 }
